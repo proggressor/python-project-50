b1fc0f239500190a74cd359ecff48afd8ea4fe14
Vorobyev <nikos1997@yandex.ru>
2023-02-19 16:02:12 +0300

    fix tests paths

--- tests/test_flat.py
+++ tests/test_flat.py
@@ -1,16 +1,12 @@
 import pytest
-from os.path import join as opj
 from . import test_results
 from gendiff.generate_diff import generate_diff
 
 
-full_path = '/home/proggressor/python-project-50/tests/fixtures'
-
-
 @pytest.mark.parametrize("file_path1,file_path2,expected", [
-    (opj(full_path, 'test_file1.json'), opj(full_path, 'test_file2.json'),
+    ('tests/fixtures/test_file1.json', 'tests/fixtures/test_file2.json',
      test_results.flat_file1),
-    (opj(full_path, 'test_file3.json'), opj(full_path, 'test_file4.json'),
+    ('tests/fixtures/test_file3.json', 'tests/fixtures/test_file4.json',
      test_results.flat_file2),
 ])
 def test_json(file_path1, file_path2, expected):
@@ -18,9 +14,9 @@ def test_json(file_path1, file_path2, expected):
 
 
 @pytest.mark.parametrize("file_path1,file_path2,expected", [
-    (opj(full_path, 'test_file1.yml'), opj(full_path, 'test_file2.yml'),
+    ('tests/fixtures/test_file1.yml', 'tests/fixtures/test_file2.yml',
      test_results.flat_file1),
-    (opj(full_path, 'test_file3.yml'), opj(full_path, 'test_file4.yml'),
+    ('tests/fixtures/test_file3.yml', 'tests/fixtures/test_file4.yml',
      test_results.flat_file2)
 ])
 def test_yaml(file_path1, file_path2, expected):
