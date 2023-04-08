cb143f906123b51f0794b5a67319ea8629b70556
Vorobyev <nikos1997@yandex.ru>
2023-04-08 20:45:08 +0300

    fix false constant

--- gendiff/parser.py
+++ gendiff/parser.py
@@ -3,31 +3,24 @@ import yaml
 import os
 
 
-EXTENSIONS = {
-    '.json': None,
-    '.yml': None,
-    '.yaml': None,
-}
-
-
 def load_json(content):
     return json.loads(content)
-
-
-EXTENSIONS['.json'] = load_json
 
 
 def load_yaml(content):
     return yaml.safe_load(content)
 
 
-EXTENSIONS['.yml'] = load_yaml
-EXTENSIONS['.yaml'] = load_yaml
+extensions = {
+    '.json': load_json,
+    '.yml': load_yaml,
+    '.yaml': load_yaml,
+}
 
 
 def read_file(file_path):
     _, file_extension = os.path.splitext(file_path)
-    if file_extension not in EXTENSIONS.keys():
+    if file_extension not in extensions.keys():
         raise Exception(f'File {file_path} has an invalid extension. '
                         f'Please select JSON-file or YAML-file')
     with open(file_path) as f:
@@ -35,4 +28,4 @@ def read_file(file_path):
 
 
 def parse(file_extension, content):
-    return EXTENSIONS.get(file_extension)(content)
+    return extensions.get(file_extension)(content)
